75f3c30fc3d7b3de5f4c4631d3f5a6170cf2bd1c
Irina  Secrii <irinabotu@Irinas-MacBook-Pro.local>
2021-12-13 19:37:51 -0600

    Module

--- main.tf
+++ main.tf
@@ -4,26 +4,101 @@ provider "aws" {
 
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
-  tags       = var.tags
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
-  tags   = var.tags
 }
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags   = var.tags
+resource "aws_subnet" "public_subnet" {
+  count                   = 3
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.public_az[count.index]
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = 3
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr[count.index]
+  availability_zone = var.private_az[count.index]
+}
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = var.cidr_block
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-resource "aws_route_table_association" "rt_assoc" {
+resource "aws_route_table" "rt_private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.cidr_block
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc_public" {
   count          = length(var.public_subnet_cidr)
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
-  route_table_id = aws_route_table.my_rt.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "rt_assoc_private" {
+  count          = length(var.private_subnet_cidr)
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.rt_private.id
+}
+
+resource "aws_security_group" "sg_web" {
+  name        = "sg_web"
+  description = "Allow SSH HTTP HTTPS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = var.protocol
+      cidr_blocks = [var.cidr_block]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = var.protocol
+      cidr_blocks = [var.cidr_block]
+    }
+  }
+}
+
+resource "aws_security_group" "sg_db" {
+  name = "sg_db"
+  description = "RDS postgres servers"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
