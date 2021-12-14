14d7e17a05ed62d91a2a8958a282ecf7498dcf5d
lbugaian <leonid.bugaian@deutsche-boerse.com>
2021-12-13 20:10:17 -0600

    vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr
-}
+#resource "aws_vpc" "vpc" {
+#  cidr_block = var.vpc_cidr
+#}
 /*
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
